98d8504f3a17ca8f57d8edebe8aec493877669e4
Thibault Vacheron <thib.du-42@hotmail.fr>
2019-04-01 22:04:03 +0200

    header et la carte bordel smartgit de merde

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Album') }}</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    @yield('css')
+</head>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <a href="{{asset('/home')}}"><img class="logo" src="{{asset('/img/Leboncoincoin.png')}}"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <ul class="navbar-nav ml-auto">
+    	
+    	<li class=""><a class="nav-link" href="{{asset('/home')}}"><img class="plus" src="{{asset('/img/+.png')}}">Désposer une annonce</a></li>
+       
+    
+    </ul>
+    
+    <ul class="navbar-nav ml-auto">
+    	
+    	
+        <li class=""><a class="nav-link" href="{{asset('/home')}}">Recherche</a></li>
+    
+    </ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+        
+            @guest
+            <li class="nav-item{{ currentRoute(route('login')) }}"><a class="nav-link" href="{{ route('login') }}">@lang('Connexion')</a></li>
+            <li class="nav-item{{ currentRoute(route('register')) }}"><a class="nav-link" href="{{ route('register') }}">@lang('Inscription')</a></li>
+            @else
+                <li class="nav-item">
+                    <a id="logout" class="nav-link" href="{{ route('logout') }}">@lang('Déconnexion')</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hide">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+            @endguest
+        </ul>
+    </div>
+</nav>
+
+@yield('content')
+
+<script src="{{ asset('js/app.js') }}"></script>
+
+@yield('script')
+<script>
+    $(() => {
+        $('#logout').click((e) => {
+            e.preventDefault()
+            $('#logout-form').submit()
+        })
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
+<script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
