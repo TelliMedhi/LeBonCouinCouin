99d5f2cb7de46571cc409af637f7301ec74be88c
Thibault Vacheron <thib.du-42@hotmail.fr>
2019-04-12 14:29:41 +0200

    petit canard en haut de l'url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Album') }}</title>
+    <title>LeBoncoincoin</title>
+     <link rel="shortcut icon" type="image/x-icon" href="{{asset('/img/url.png')}}" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styleFooter.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
